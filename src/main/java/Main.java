f11f07e3123976f5358b6352389e695ff25f9fb3
JesusOrtega <jesusortegadc@gmail.com>
2022-09-06 11:45:24 -0700

    Wrong implementation of OCP

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args){
+        
+        HospitalManagement ERDirector = new HospitalManagement();
+
+        Employee pepa = new Nurse(1, "Pepa", "Emergency", true);
+        ERDirector.callUpon(pepa);
+
+        Employee rox = new Doctor(2, "Roxy", "Emergency", true);
+        ERDirector.callUpon(rox);
+
+    }
+
+}
